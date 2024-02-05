005b1d54531a4288fba665a2c100e5bb1010f454
Kabkav <158855852+Kabkav@users.noreply.github.com>
2024-02-05 16:25:29 +0300

    Update owner.js

--- src/commands/bot/owner.js
+++ src/commands/bot/owner.js
@@ -3,26 +3,26 @@ const Discord = require('discord.js');
 module.exports = async (client, interaction, args) => {
     client.embed({
         title: `📘・Owner information`,
-        desc: `____________________________`,
+        desc: `--Semih Kutsal`,
         thumbnail: client.user.avatarURL({ dynamic: true, size: 1024 }),
         fields: [{
             name: "👑┆Owner name",
-            value: `Corwin`,
+            value: `"H"`,
             inline: true,
         },
         {
             name: "🏷┆Discord tag",
-            value: `</Corwin>#0001`,
+            value: `</Kabkav>#0`,
             inline: true,
         },
         {
             name: "🏢┆Organization",
-            value: `CoreWare`,
+            value: `Santo`,
             inline: true,
         },
         {
             name: "🌐┆Website",
-            value: `[https://corwindev.nl](https://corwindev.nl)`,
+            value: `[https://linktr.ee/therealmofsanto]`,
             inline: true,
         }],
         type: 'editreply'
