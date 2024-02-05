f37bd33729ecd005fd64412555860d8d74dd3a88
Kabkav <158855852+Kabkav@users.noreply.github.com>
2024-02-05 16:28:19 +0300

    Update bot.js

--- src/config/bot.js
+++ src/config/bot.js
@@ -8,9 +8,9 @@ module.exports = {
     discord: {
         id: process.env.DISCORD_ID,
         prefix: '!',
-        footer: `© Corwin 2021 - ${new Date().getFullYear()}`, 
+        footer: `© SANTO 2023 - ${new Date().getFullYear()}`, 
         botInvite: `https://discord.com/oauth2/authorize?&client_id=840212110817755157&scope=applications.commands+bot&permissions=8`,
-        serverInvite: "https://discord.gg/qHXxpQKC2H",
+        serverInvite: "https://discord.gg/e96j4j5wjb",
     },
 
     wordList: `Airplane
